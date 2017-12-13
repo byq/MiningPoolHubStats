ccb7b62b0fec28e59007abad45fb191c66abeb51
James Dimitrov <james.dimitrov@egov.com>
2017-12-13 15:40:04 -0600

    added table sorting and filtering css file

--- minerstats.php
+++ minerstats.php
@@ -69,6 +69,8 @@ $crypto_decimals = $mph_stats->get_decimal_for_conversion();
     <meta name="author" content="">
     <title>Miner Stats</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+
     <style>
         body {
             padding-top: 4.5rem;
